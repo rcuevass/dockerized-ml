a610c994621b614c208b3bc7542e7dd5b2eef6ae
rcuevass <rogercuevas.ca@gmail.com>
2020-11-07 13:54:21 -0500

    adding documenting details to README; some changes to training.py

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Dockerized ML
 
 Repo to exemplify dockerization of ML models. 
+
+Key points:
+
+1. Build your Docker image by typing in from main folder: `docker build -t basic_clf:1.0 . ` This will create 
+an image named `basic_clf` with tag `1.0`
+
+2. Upon successful image built, confirm your image has indeed been created by typing in `docker images`. Information
+about your image should be displayed. 
+
+3. To run the docker image just created, type in `docker run --name deployedCLF -p 8080:8080 basic_clf:1.0`. 
+Here *deployedCLF* will be the name of the container.
+
+4. Upon execution of the command above you can check your docker container is running by typing in `docker ps` 
+
+**Important**. The main goal of this repo is to exemplify the process of dockerizing the ML algorithm. Thus, the model
+deployed may not be sound in terms of performance - it will be refined as this repo is maintained
+
--- model/training/training.py
+++ model/training/training.py
@@ -7,13 +7,13 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble.gradient_boosting import GradientBoostingClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
+from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 import pickle
 
 warnings.filterwarnings('ignore')
 warnings.filterwarnings('ignore', category=DeprecationWarning)
 
-# Data Source: https://data.world/abelvikas/diabetes-type-dataset
 df = pd.read_csv('../../data/input/diabetestype.csv')
 
 # Dropping the class feature
@@ -24,24 +24,29 @@ y = df.iloc[:, -1]
 
 
 def compute_score(clf, X, y, scoring='accuracy'):
-    xval = cross_val_score(clf, X, y, cv = 5, scoring=scoring)
-    return np.mean(xval)
+    xval = cross_val_score(clf, X, y, cv=10, scoring=scoring)
+    return np.mean(xval), np.std(xval)
 
 
 # instantiate different base models
 
 logreg = LogisticRegression()
 logreg_cv = LogisticRegressionCV()
-rf = RandomForestClassifier(n_estimators=20, max_features='sqrt')
-gboost = GradientBoostingClassifier()
-models = [logreg, logreg_cv, rf, gboost]
+svm = SVC()
 
 
-for model in models:
-    print('Cross-validation of : {0}'.format(model.__class__))
-    score = compute_score(clf=model, X=X, y=y, scoring='accuracy')
-    print('CV score = {0}'.format(score))
-    print('****')
+# create dictionary
+dict_models = {'LR': logreg, 'LRCV': logreg_cv, 'SVM': svm}
+
+
+for model_name in dict_models.keys():
+    model_obj = dict_models[model_name]
+    print('Cross-validation of : {0}'.format(model_obj.__class__))
+    score = compute_score(clf=model_obj, X=X, y=y, scoring='accuracy')
+    print('Algorithm ', model_name)
+    print('CV score mean = {0}'.format(score[0]))
+    print('CV score std = {0}'.format(score[1]))
+    print('========================================')
 
 
 # Lets try train test split
@@ -49,7 +54,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random
 
 
 # Using the random forest algorithm
-model = rf.fit(X_train, y_train)
+model = dict_models['LRCV'].fit(X_train, y_train)
 
 # Saving the model with pickle
 
